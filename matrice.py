778ee7c5769789b317c2f64729bf2a17b232decd
atimur1220 <aksoy.timur@moldofinlandez.md>
2026-03-25 11:55:42 +0200

    slopwork (unfinished "array" thing)
    hi

---
+++ matrice.py
@@ -0,0 +1,6 @@
+import array as arr
+nr = int(input("introduceti numarul de elemente: "))
+matrice = []
+for i in range(nr):
+    linie = list(map(int,input().split()))
+    matrice.append() #i refuse to comment this code 
